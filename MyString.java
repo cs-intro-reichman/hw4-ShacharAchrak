cb1917115413f5f8871f6309704951d21d03910f
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-12-02 16:34:29 +0200

    MyString

--- MyString.java
+++ MyString.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class MyString {
     public static void main(String[] args) {
         System.out.println("Testing lowercase:");
@@ -18,13 +22,43 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String s ="";
+        for(int i = 0; i < str.length(); i++){
+			char c = str.charAt(i);
+			if(c >='A' && c <= 'Z'){
+					c = (char) (c +'a'-'A');
+				}
+                s += c;
+            }
+        return s;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        str1 = lowerCase(str1);
+        str2 = lowerCase(str2);
+        
+        if (str1 == null || str2 == null){
+            return false;
+        }
+        if(str2.length()==0){
+            return true;
+        }
+        if(str1.length() < str2.length()){
+            return false;
+        }
+
+        for(int i=0; i<str1.length() - str2.length(); i++){
+            int j = 0;
+            for( j=0; j<str2.length(); j++){
+                if(str1.charAt(i+j) != str2.charAt(j)){
+                    break;
+                }
+            }
+                if( j == str2.length()){
+                    return true;
+                }
+            }
         return false;
     }
 }
